ab90cd9484ee67ffe36925e1f43ce45effd4a24c
Jimmy Klein <jimmy.klein4@gmail.com>
2020-10-19 09:03:23 -0400

    fixed the dumb date

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if(config.timer == '') {
 client.on('message', message => {
 	const regex = /(\sword\s|^word\s|\sword$|^word$)/;
 	// 60,000 is amount of ms in a minute
-	if(regex.test(message.content) && Date.now - config.timer > config.cooldownTimerMinutes * 60000) {
+	if(regex.test(message.content) && Date.now() - config.timer > config.cooldownTimerMinutes * 60000) {
 		const timeSinceLastMessage = Date.now() - config.timer;
 		const responseMessage = require('./messageFormatter.js').getFormattedMessage(timeSinceLastMessage);
 		message.channel.send(responseMessage);
