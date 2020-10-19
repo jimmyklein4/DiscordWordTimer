b495882950d9224592e9cfb80026b9adb562c3cf
Jimmy Klein <jimmy.klein4@gmail.com>
2020-10-18 21:48:53 -0400

    Cleaned some stuff up and added regex

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const config = require('./config.json')
+
+const config = require('./config.json');
+const auth = require('./auth.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-  console.log('ready!');
+	console.log('ready!');
 });
+
+if(config.timer == '') {
+	config.timer = Date.now();
+	fs.writeFile('config.json', JSON.stringify(config), function(err) {
+		if(err) {
+			console.log(err);
+		}
+	});
+}
 
 client.on('message', message => {
-  if(message.content === '!ping'){
-    message.channel.send('Pong');
-  }
+	const regex = /(\scum\s|^cum\s|\scum$|^cum$)/;
+	if(regex.test(message.content)) {
+		const timeSinceLastCum = Date.now() - config.timer;
+		const responseMessage = require('./messageFormatter.js').getFormattedMessage(timeSinceLastCum);
+		message.channel.send(responseMessage);
+		config.timer = Date.now();
+		fs.writeFile('config.json', JSON.stringify(config), function(err) {
+			if(err) {
+				console.log(err);
+			}
+		});
+	}
 });
 
-client.login(config.token);
+client.login(auth.token);
