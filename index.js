3b01296e0a53260b6b0e5edc09383a9dd4795c97
Jimmy Klein <jimmy.klein4@gmail.com>
2022-08-31 14:44:09 -0400

    ES6 reformat

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const fs = require('fs');
-const { Client, Intents } = require('discord.js');
+import { Client, Intents } from 'discord.js';
+import { readFileSync } from 'fs';
+// import token  from './auth.json' assert {type:'json'};
+const token = JSON.parse(readFileSync('./auth.json'));
 
-const config = require('./config.json');
-const auth = require('./auth.json');
+import { createMessage } from './messageCreator.js';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', c => {
@@ -10,47 +11,7 @@ client.once('ready', c => {
 	console.log(client.guilds);
 });
 
-client.on('messageCreate', message => {
-	console.log(message);
-	if (message.author.bot) {
-		return;
-	}
+// TODO: Move this to its own method/file or something
+client.on('messageCreate', message => createMessage(message));
 
-	let haveGuildInConfig = false;
-	for (let i = 0; i < config.guilds.length; i++) {
-		if (config.guilds[i].id === message.guildId) {
-			let searchWord = config.guilds[i].searchWord;
-			console.log(searchWord);
-			if(!searchWord) {
-				searchWord = 'test';
-			}
-			const regex = new RegExp('\\s' + searchWord + '\\s|^' + searchWord + '\\s|\\s' + searchWord + '$|^' + searchWord + '$|\\s' + searchWord + '\\W|^' + searchWord + '\\W', 'i');
-			// const regex = /(\sword\s|^word\s|\sword$|^word$|\sword\W|^word\W)/i;
-			haveGuildInConfig = true;
-			// 60,000 is amount of ms in a minute
-			console.log(regex);
-			if (regex.test(message.content) && Date.now() - config.guilds[i].timer > config.guilds[i].cooldownTimerMinutes * 60000) {
-				const timeSinceLastMessage = Date.now() - config.guilds[i].timer;
-				const responseMessage = require('./messageFormatter.js').getFormattedMessage(timeSinceLastMessage, searchWord);
-				message.channel.send(responseMessage);
-				config.guilds[i].timer = Date.now();
-				fs.writeFile('config.json', JSON.stringify(config), function(err) {
-					if (err) {
-						console.log(err);
-					}
-				});
-			}
-		}
-	}
-
-	if (!haveGuildInConfig) {
-		config.guilds.push({ id : message.guildId, prefix: '!', timer: Date.now(), cooldownTimerMinutes: 1 });
-		fs.writeFile('config.json', JSON.stringify(config), function(err) {
-			if (err) {
-				console.log(err);
-			}
-		});
-	}
-});
-
-client.login(auth.token);
+client.login(token.token);
---
+++ messageCreator.js
@@ -0,0 +1,52 @@
+import { readFileSync } from 'fs';
+
+// import config from './config.json' assert{type: 'json'};
+import { writeFile } from 'fs';
+import { getFormattedMessage } from './messageFormatter.js';
+
+const config = JSON.parse(readFileSync('./config.json'));
+
+export function createMessage(message) {
+    console.log(message);
+	if (message.author.bot) {
+		return;
+	}
+
+	let haveGuildInConfig = false;
+	for (let i = 0; i < config.guilds.length; i++) {
+		if (config.guilds[i].id === message.guildId) {
+			let searchWord = config.guilds[i].searchWord;
+			console.log(searchWord);
+			if(!searchWord) {
+				searchWord = 'test';
+			}
+			const regex = new RegExp('\\s' + searchWord + '\\s|^' + searchWord + '\\s|\\s' + searchWord + '$|^' + searchWord + '$|\\s' + searchWord + '\\W|^' + searchWord + '\\W', 'i');
+			// const regex = /(\sword\s|^word\s|\sword$|^word$|\sword\W|^word\W)/i;
+			haveGuildInConfig = true;
+			// 60,000 is amount of ms in a minute
+			console.log(regex);
+			if (regex.test(message.content) && Date.now() - config.guilds[i].timer > config.guilds[i].cooldownTimerMinutes * 60000) {
+				const timeSinceLastMessage = Date.now() - config.guilds[i].timer;
+				const responseMessage = getFormattedMessage(timeSinceLastMessage, searchWord);
+				message.channel.send(responseMessage);
+				config.guilds[i].timer = Date.now();
+				writeFile('config.json', JSON.stringify(config), function(err) {
+					if (err) {
+						console.log(err);
+					}
+				});
+			}
+		}
+	}
+
+	if (!haveGuildInConfig) {
+		// TODO: Need to add search word config in here
+		guilds.push({ id : message.guildId, prefix: '!', timer: Date.now(), cooldownTimerMinutes: 1 });
+		writeFile('config.json', JSON.stringify(config), function(err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+	}
+
+}
