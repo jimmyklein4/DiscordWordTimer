6f233f3860f72f3caa6f7be7f6b430e972bad310
Jimmy Klein <jimmy.klein4@gmail.com>
2022-09-06 17:33:46 -0400

    Moved config call

--- commands/wordTimeTracker.js
+++ commands/wordTimeTracker.js
@@ -1,12 +1,12 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { readFileSync } from 'fs';
 import { formattedTime } from '../messageFormatter.js';
-const config = JSON.parse(readFileSync('./config.json'));
 
 export const data = new SlashCommandBuilder()
     .setName('currentwordrecord')
     .setDescription('Longest time target word has not been said');
 export async function execute(interaction) {
+    const config = JSON.parse(readFileSync('./config.json'));
     for(let i = 0; i < config.guilds.length; i++) {
         if (config.guilds[i].id === interaction.guildId) {
             const longestTime = config.guilds[i].longesttime;
